a44a601d42cca917caf957c7c9d185d2dbf3f08d
Pavan <adhavpavan@gmail.com>
2023-05-25 22:39:25 +0530

    caliper integration added

--- artifacts/src/github.com/fabcar/go/fabcar.go
+++ artifacts/src/github.com/fabcar/go/fabcar.go
@@ -27,12 +27,204 @@ type Car struct {
 }
 
 type SensorData struct {
-	ID string `json:"id"`
-	DataId string `json:"dataId"`
+	ID         string `json:"id"`
+	DataId     string `json:"dataId"`
 	RecordDate string `json:"recordDate"`
-	RoomId string `json:"roomId"`
-	Temp string `json:"temp"`
-	InOut string `json:"inOut"`
+	RoomId     string `json:"roomId"`
+	Temp       string `json:"temp"`
+	InOut      string `json:"inOut"`
+}
+
+type Asset struct {
+	AppraisedValue int    `json:"AppraisedValue"`
+	Department     string `json:"Department"`
+	ID             string `json:"ID"`
+	Patient        string `json:"Patient"`
+	Amount         int    `json:"Amount"`
+}
+
+// InitLedger adds a base set of assets to the ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	assets := []Asset{}
+	count := 10000
+	departments := []string{
+		"Xray",
+		"BloodCheck",
+		"Consultation",
+		"MRI",
+		"Inpatient",
+		"Outpatient",
+	}
+	names := []string{
+		"Tomoko",
+		"rBradd",
+		"Jin",
+		"Max",
+		"Adriana",
+		"Michel",
+	}
+
+	for i := 1; i <= count; i++ {
+		assets = append(assets, Asset{ID: fmt.Sprint("asset", i), Department: departments[i%6], Amount: 5 * i, Patient: names[i%6], AppraisedValue: 300 + i})
+
+	}
+	for _, asset := range assets {
+		assetJSON, err := json.Marshal(asset)
+		if err != nil {
+			return err
+		}
+
+		err = ctx.GetStub().PutState(asset.ID, assetJSON)
+		if err != nil {
+			return fmt.Errorf("failed to put to world state. %v", err)
+		}
+	}
+
+	return nil
+}
+
+// CreateAsset issues a new asset to the world state with given details.
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, department string, amount int, patient string, appraisedValue int) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", id)
+	}
+
+	asset := Asset{
+		ID:             id,
+		Department:     department,
+		Amount:         amount,
+		Patient:        patient,
+		AppraisedValue: appraisedValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// ReadAsset returns the asset stored in the world state with given id.
+func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &asset, nil
+}
+
+// UpdateAsset updates an existing asset in the world state with provided parameters.
+func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, department string, amount int, patient string, appraisedValue int) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	// overwriting original asset with new asset
+	asset := Asset{
+		ID:             id,
+		Department:     department,
+		Amount:         amount,
+		Patient:        patient,
+		AppraisedValue: appraisedValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// DeleteAsset deletes an given asset from the world state.
+func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	return ctx.GetStub().DelState(id)
+}
+
+// AssetExists returns true when asset with given ID exists in world state
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return assetJSON != nil, nil
+}
+
+// TransferAsset updates the patient field of asset with given id in world state, and returns the old patient.
+func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newPatient string) (string, error) {
+	asset, err := s.ReadAsset(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	oldPatient := asset.Patient
+	asset.Patient = newPatient
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return "", err
+	}
+
+	err = ctx.GetStub().PutState(id, assetJSON)
+	if err != nil {
+		return "", err
+	}
+
+	return oldPatient, nil
+}
+
+// GetAllAssets returns all assets found in world state
+func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var assets []*Asset
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
+	}
+
+	return assets, nil
 }
 
 func (s *SmartContract) CreateSensorData(ctx contractapi.TransactionContextInterface, sensorData string) (string, error) {
@@ -66,7 +258,6 @@ func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, c
 	if err != nil {
 		return "", fmt.Errorf("Failed while marshling car. %s", err.Error())
 	}
-
 
 	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(car.ID, carAsBytes)
 }
